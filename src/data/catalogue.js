281c55cc27f7f417a9f5f28f34b59d59078f6b23
Ron Mathew Sam <ronsam98@gmail.com>
2020-07-09 23:08:26 +0530

    latest v1

--- src/data/catalogue.js
+++ src/data/catalogue.js
@@ -11,19 +11,19 @@ let catalogue = [{
     },
     {
         "title": "Shakes and Smoothies Straws",
-        "specs": "Diameter: .314” Length Range-7”-12”",
+        "specs": "Diameter: 0.314” Length Range: 7”-12”",
         "images": [Shakes],
         "description": "Whether its your favorite chocolate peanut butter banana shake or your high protein fruit smoothie,  is the ideal pick for you.  The .31” inner diameter gives you an easy swooshy slurp up without grabbing the eye balls of your neighbours…  while you immerse yourself in the flavory pool of smooth(y) experience. Ideal for your cold coffee, too."
     },
     {
         "title": "Cocktail Straws",
-        "specs": "Diameter:236” Length-5.75”",
+        "specs": "Diameter: 0.236” Length: 5.75”",
         "images": [Cocktail],
         "description": "Cocktail straws are specially customized to suit your low ball glasses of cocktails be it your mixed drinks of juices and spirits."
     },
     {
         "title": "BobaTea Straws",
-        "specs": "Diameter. 0.511” Length Range-7”-12”",
+        "specs": "Diameter. 0.511” Length Range: 7”-12”",
         "images": [Boba],
         "description": "The BobaTea straw is thoughtfully designed wide enough to facilitate an uninterrupted profusion of the luscious chewy tapioca pearls for your palate while you sink into the flavours of your choice black or green pearl milk boba tea.<br/> And rest assured, your BobaTea straw is audaciously equipped with a pointed tip to pierce the seal on your tea jar or glass."
     }
